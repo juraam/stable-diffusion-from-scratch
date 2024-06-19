76abe5b0d635b93b73bcfc2b3fc89eee5cf93d83
Yuriy Moshkov <juramoshkov@gmail.com>
2024-06-19 14:10:28 +0200

    Updated down block and up blocks, added last conv in unet

---
+++ src/utils.py
@@ -0,0 +1,14 @@
+import random, os
+import numpy as np
+import torch
+
+
+def seed_everything(seed):   
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    if seed is None:
+        torch.manual_seed(torch.initial_seed())
+        torch.cuda.manual_seed(torch.initial_seed())
+        torch.backends.cudnn.deterministic = False
+        torch.backends.cudnn.benchmark = False
